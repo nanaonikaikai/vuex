ae024ebb6d9cf7f32c5c5f30f506bcbdfd89a0fc
kaikai <m13339490373@163.com>
2018-08-29 18:30:18 +0800

    添加 count

--- vuex/src/components/count.vue
+++ vuex/src/components/count.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>{{msg}}</h2>
         <hr/>
-        <h3>{{$store.state.count}}-{{count}}</h3>
+        <h3>{{$store.state.count}}-{{count}}-{{count}}}</h3>
         <div>
     <button @click="$store.commit('add',+10)">+</button>
     <button @click='reduce'>-</button>
